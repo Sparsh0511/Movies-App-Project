3070040f8370cdb5d040e461efe36274cca88c49
Sparsh <ssparsh100@gmail.com>
2025-01-10 12:15:28 +0530

    Popular and upcoming movies list with deatils of every movie

---
+++ app/src/main/java/com/ahmedapps/moviesapp/core/presentation/UpcomingMoviesScreen.kt
@@ -0,0 +1,55 @@
+package com.ahmedapps.moviesapp.core.presentation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.ahmedapps.moviesapp.core.presentation.components.MovieItem
+import com.ahmedapps.moviesapp.movieList.presentaion.MovieListState
+import com.ahmedapps.moviesapp.movieList.presentaion.MovieListUiEvent
+import com.ahmedapps.moviesapp.movieList.util.Category
+
+@Composable
+fun UpcomingMoviesScreen(
+    movieStateList: MovieListState,
+    navHostController: NavHostController,
+    onEvent: (MovieListUiEvent) -> Unit
+) {
+    if(movieStateList.upcomingMovieList.isEmpty()) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ) {
+            CircularProgressIndicator()
+        }
+    }
+    else {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 8.dp, horizontal = 4.dp)
+        ) {
+            items(movieStateList.upcomingMovieList.size) { index ->
+                MovieItem(
+                    movie = movieStateList.upcomingMovieList[index],
+                    navHostController = navHostController
+                )
+
+                Spacer(Modifier.height(16.dp))
+
+                if(index >= movieStateList.upcomingMovieList.size-1 && !movieStateList.isLoading ) {
+                    onEvent(MovieListUiEvent.Paginate(Category.UPCOMING))
+                }
+            }
+        }
+    }
+}
